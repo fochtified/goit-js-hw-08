7615e541f9d2390abd8f0473231b6d5d0cbd0b28
Grzegorz Focht <douslug.to@gmail.com>
2024-05-31 22:17:36 +0200

    Ready for evaluation

--- js/task-1.js
+++ js/task-1.js
@@ -1,11 +1,26 @@
-function slugify(title) {
-let titleLowerCase = title.toLowerCase();
-let titleWhole = titleLowerCase.split(` `);
-let titleSlug = titleWhole.join(`-`);
-return titleSlug;
+function isEnoughCapacity(products, containerSize) {
+let sum = 0;
+const product = Object.values(products)
+for (let i = 0; i < product.length; i += 1 ){
+    sum += product[i]
 }
 
-console.log(slugify("Arrays for begginers")); // "arrays-for-begginers"
-console.log(slugify("English for developer")); // "english-for-developer"
-console.log(slugify("Ten secrets of JavaScript")); // "ten-secrets-of-javascript"
-console.log(slugify("How to become a JUNIOR developer in TWO WEEKS")); // "how-to-become-a-junior-developer-in-two-weeks"
+if (sum < containerSize){
+    return true
+   }
+   else 
+    return false
+}
+
+console.log(
+    isEnoughCapacity({ apples: 2, grapes: 3, carrots: 1 }, 8)
+  ); // true
+  console.log(
+    isEnoughCapacity({ apples: 4, grapes: 6, lime: 16 }, 12)
+  ); // false
+  console.log(
+    isEnoughCapacity({ apples: 1, lime: 5, tomatos: 3 }, 14)
+  ); // true
+  console.log(
+    isEnoughCapacity({ apples: 18, potatos: 5, oranges: 2 }, 7)
+  ); // false 
