0d9002cb8f4558aa17aaf13d6a5f4c792185dc26
Grzegorz Focht <douslug.to@gmail.com>
2024-06-27 20:36:16 +0200

    upd

--- js/task-3.js
+++ js/task-3.js
@@ -1,37 +1,7 @@
-"use strict";
+const nameInput = document.getElementById("name-input");
+const nameOutput = document.getElementById("name-output");
 
-
-class StringBuilder {
-  #value;
-
-  constructor(initialValue) {
-    this.#value = initialValue;
-  }
-
-  getValue() {
-    return this.#value;
-  }
-  
-  padEnd(str) {
-    this.#value += str;
-  }
-
-  padStart(str) {
-    this.#value = str + this.#value;
-  }
-
-  padBoth(str) {
-    this.#value = str + this.#value + str;
-  }
-}
-
-
-
-const builder = new StringBuilder(".");
-console.log(builder.getValue()); // "."
-builder.padStart("^");
-console.log(builder.getValue()); // "^."
-builder.padEnd("^");
-console.log(builder.getValue()); // "^.^"
-builder.padBoth("=");
-console.log(builder.getValue()); // "=^.^="
+nameInput.addEventListener("input", () => {
+  const name = nameInput.value.trim();
+  nameOutput.textContent = name ? name : "Anonymous";
+});
