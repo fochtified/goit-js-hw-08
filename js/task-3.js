ddeca726448719d9488400a36901fdb1c6421e34
Grzegorz Focht <douslug.to@gmail.com>
2024-07-13 13:36:02 +0200

    Prepwork

--- js/task-3.js
+++
@@ -1,7 +0,0 @@
-const nameInput = document.getElementById("name-input");
-const nameOutput = document.getElementById("name-output");
-
-nameInput.addEventListener("input", () => {
-  const name = nameInput.value.trim();
-  nameOutput.textContent = name ? name : "Anonymous";
-});
